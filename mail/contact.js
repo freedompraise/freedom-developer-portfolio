357239cb198a818ab2ca2bb71fde5e6f8d8afe8d
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-11 11:51:24 +0100

    Refactor contact form submission to use WhatsApp API

--- mail/contact.js
+++ mail/contact.js
@@ -19,10 +19,11 @@ $(function () {
         type: "POST",
         data: {
           name: name,
-          email: email,
+          _replyto: email,
           subject: subject,
           message: message,
         },
+        dataType: "json",
         cache: false,
         success: function () {
           $("#success").html("<div class='alert alert-success'>");
@@ -62,65 +63,16 @@ $(function () {
     },
   });
 
-  $(
-    "#contactFormWhatsApp input, #contactFormWhatsApp textarea"
-  ).jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function ($form, event, errors) {},
-    submitSuccess: function ($form, event) {
-      event.preventDefault();
-      var name = $("input#name-whatsapp").val();
-      var phone = $("input#phone-whatsapp").val();
-      var message = $("textarea#message-whatsapp").val();
+  $("#contactFormWhatsApp").on("submit", function (event) {
+    event.preventDefault();
+    var name = $("input#name-whatsapp").val();
+    var phone = $("input#phone-whatsapp").val();
+    var message = $("textarea#message-whatsapp").val();
 
-      $this = $("#sendMessageWhatsAppButton");
-      $this.prop("disabled", true);
-
-      $.ajax({
-        url: "https://api.whatsapp.com/send",
-        type: "GET",
-        data: {
-          phone: "+2349074577147",
-          text:
-            "Name: " + name + "%0APhone: " + phone + "%0AMessage: " + message,
-        },
-        cache: false,
-        success: function () {
-          $("#success").html("<div class='alert alert-success'>");
-          $("#success > .alert-success")
-            .html(
-              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>"
-            )
-            .append("<strong>Your message has been sent. </strong>")
-            .append("</div>");
-          $("#contactFormWhatsApp").trigger("reset");
-        },
-        error: function () {
-          $("#success").html("<div class='alert alert-danger'>");
-          $("#success > .alert-danger")
-            .html(
-              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>"
-            )
-            .append(
-              $("<strong>").text(
-                "Sorry " +
-                  name +
-                  ", it seems that our WhatsApp server is not responding. Please try again later!"
-              )
-            )
-            .append("</div>");
-          $("#contactFormWhatsApp").trigger("reset");
-        },
-        complete: function () {
-          setTimeout(function () {
-            $this.prop("disabled", false);
-          }, 1000);
-        },
-      });
-    },
-    filter: function () {
-      return $(this).is(":visible");
-    },
+    var contactMessage = `Contact Details:\nName: ${name}\nPhone Number: ${phone}\nMessage: ${message}`;
+    var encodedMessage = encodeURIComponent(contactMessage);
+    var whatsappURL = `https://wa.me/2349074577147/?text=${encodedMessage}`;
+    window.location.href = whatsappURL;
   });
 
   $('a[data-toggle="tab"]').click(function (e) {
